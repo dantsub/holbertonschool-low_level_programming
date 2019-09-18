6a0a800e7ee79952e348466c69de1c179cf0692d
Dany <dany.mrtinez@gmail.com>
2019-09-18 20:22:29 +0000

    add comments in 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+*main functions
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
