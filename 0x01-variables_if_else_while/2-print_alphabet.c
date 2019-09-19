649f2fcf520bd3f530026fcef847e9a3e1bc670f
Dany <dany.mrtinez@gmail.com>
2019-09-19 21:59:00 +0000

    add 2-print_alphabet.c and update README.md

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = {"abcdefghijklmnopqrstuvwxyz"};
+	int size = sizeof(alphabet);
+	int start = 0;
+
+	while (start <= size)
+	{
+		putchar(alphabet[start]);
+		if (start == size)
+		{
+			putchar('\n');
+		}
+		start++;
+	}
+	return (0);
+}
