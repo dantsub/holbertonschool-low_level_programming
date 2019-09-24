b9572c7d98f0e888979927def8ac2c6ad26198e7
Dany <dany.mrtinez@gmail.com>
2019-09-24 23:22:57 +0000

    fix for betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - adds two integers and returns the result.
+ * add - adds two integers and returns the result.
  * @num1: first number.
  * @num2: second number.
  * Return: add two numbers..
