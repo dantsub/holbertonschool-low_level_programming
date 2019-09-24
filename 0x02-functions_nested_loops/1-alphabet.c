1501e945de7dfdbdf25708ca2ac122f03ee84373
Dany <dany.mrtinez@gmail.com>
2019-09-24 15:02:58 +0000

    add 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - pritn alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
