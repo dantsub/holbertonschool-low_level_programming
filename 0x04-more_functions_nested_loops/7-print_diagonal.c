8646f9514dcd6f16648a48a582b1ae98e405596a
dantsub <dany.mrtinez@gmail.com>
2019-09-27 03:11:05 +0000

    fix 6 taks and 7 taks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,12 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n > 0)
-	{	
+	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if(i == j)
+				if (i == j)
 				{
 					_putchar(92);
 					break;
