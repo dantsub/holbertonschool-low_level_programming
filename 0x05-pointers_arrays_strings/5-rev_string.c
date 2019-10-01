cc96f4259ff0b629d41cb805b73ba9fdddb7ef57
Dany <dany.mrtinez@gmail.com>
2019-10-01 23:42:31 +0000

    fix for betty rules

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@
   */
 void rev_string(char *s)
 {
-	int len,index;
+	int len, index;
 	char *arr;
 
 	while (s[len] != 0)
@@ -16,7 +16,7 @@ void rev_string(char *s)
 		len++;
 	}
 	arr = (char *) malloc(len * sizeof(char));
-	for(index = 0; index <= len; index++)
+	for (index = 0; index <= len; index++)
 	{
 		arr[index] = (char) *(s + index);
 	}
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 {
 	while (*str != 0)
 	{	_putchar(*str);
-		str+= 2;
+		str += 2;
 	}
 	_putchar(10);
 }
