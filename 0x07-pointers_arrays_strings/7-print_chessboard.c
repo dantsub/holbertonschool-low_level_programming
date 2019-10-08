f87008b19e980e155fea5f34e2f03ad987cb0ebc
Dany <dany.mrtinez@gmail.com>
2019-10-08 22:21:50 +0000

    add 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *
+  *
+  *
+  *
+  */
+void print_chessboard(char (*a)[8])
+{
+	int index, index2;
+
+	for (index = 0; index < 8; index++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[index][index2]);
+		_putchar(10);
+	}
+}
