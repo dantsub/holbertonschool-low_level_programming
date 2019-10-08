616a2fb5a665392a8f434c56098b5054eb2b5051
Dany <dany.mrtinez@gmail.com>
2019-10-08 02:09:47 +0000

    add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * _strspn - gets the length of a prefix substring.
+  * @s: first char.
+  * @accept: second char.
+  *
+  * Return: count.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int index, index2, count;
+
+	for (index = 0; s[index] != 0; index++)
+	{
+		if (s[index] == ' ')
+		{
+			break;
+		}
+		else
+			for (index2 = 0; accept[index2] != 0; index2++)
+				if (s[index] == accept[index2])
+					count++;
+	}
+	return (count);
+}
