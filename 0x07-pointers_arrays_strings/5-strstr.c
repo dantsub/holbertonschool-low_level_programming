3e13f52e84534c8567027a84f0704b92a1223e7c
Dany <dany.mrtinez@gmail.com>
2019-10-09 00:33:38 +0000

    bit fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,13 +4,13 @@
   * _strstr - function that locates a substring.
   * @haystack: char pointer
   * @needle: char pointer.
-  *
+  * Return: null or string.
   */
 char *_strstr(char *haystack, char *needle)
 {
 	int index, index1, index2;
 
-	for (index = 0; haystack[index] != '\0'; index++)
+	for (index = 0; haystack[index] != 0; index++)
 	{
 		for (index2 = index, index1 = 0; needle[index1] != 0; index1++, index2++)
 			if (haystack[index2] != needle[index1] || haystack[index2] == 0)
