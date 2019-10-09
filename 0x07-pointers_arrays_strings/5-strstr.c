f2466f9a32ff579121a5313f8351cf09a65059f4
Dany <dany.mrtinez@gmail.com>
2019-10-09 00:31:49 +0000

    add 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * _strstr - function that locates a substring.
+  * @haystack: char pointer
+  * @needle: char pointer.
+  *
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int index, index1, index2;
+
+	for (index = 0; haystack[index] != '\0'; index++)
+	{
+		for (index2 = index, index1 = 0; needle[index1] != 0; index1++, index2++)
+			if (haystack[index2] != needle[index1] || haystack[index2] == 0)
+				break;
+		if (needle[index1] == 0)
+			return (haystack + index);
+	}
+	return (NULL);
+}
