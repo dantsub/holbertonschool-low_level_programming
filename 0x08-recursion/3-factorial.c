f88ad671908d98b5a885d78a009f9ab36eadd31b
Dany <dany.mrtinez@gmail.com>
2019-10-09 21:56:31 +0000

    factorial task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * factorial - factorial of a given number.
+  * @n: number.
+  *
+  * Return: factorial.
+  */
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
