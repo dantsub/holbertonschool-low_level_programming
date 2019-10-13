857e66257667e731106e504b67afe29d482ce5c7
Dany <dany.mrtinez@gmail.com>
2019-10-13 01:12:50 +0000

    add 4-add.c incomplite

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - adds positive numbers.
+  * @argc: int.
+  * @argv: parameter type array.
+  * Return: 0.
+  */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
