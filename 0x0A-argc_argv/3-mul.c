653a1292c103f05c21235b4ada7feab983d4e38f
Dany <dany.mrtinez@gmail.com>
2019-10-13 00:29:31 +0000

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - multiplies two numbers.
+  * @argc: int.
+  * @argv: parameter type array.
+  * Return: 0.
+  */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2;
+
+	if (argc > 2)
+	{
+		arg1 = atoi(argv[1]);
+		arg2 = atoi(argv[2]);
+		printf("%d\n",arg1 * arg2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
