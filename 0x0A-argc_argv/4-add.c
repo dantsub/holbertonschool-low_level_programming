8343a02898be0e06af54ab2aa8993f0c067b00d4
Dany <33495291+dantsub@users.noreply.github.com>
2019-10-13 20:26:33 -0500

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
   * main - adds positive numbers.
   * @argc: int.
   * @argv: parameter type array.
-  * Return: 0.
+  * Return: 0 or 1.
   */
 int main(int argc, char *argv[])
 {
-
+	int add, count;
+	
+	count = 1;
+	sum = 0;
+	
+	while (count < argc)
+	{
+		if (!isdigit(argv[count]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		add += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n",add);
 	return (0);
 }
