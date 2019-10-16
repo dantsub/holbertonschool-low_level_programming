943f98f6033d275af4421db74f480735cb20842b
Dany <dany.mrtinez@gmail.com>
2019-10-16 23:27:36 +0000

    update 5 taks

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -9,12 +9,35 @@
   */
 char *argstostr(int ac, char **av)
 {
+	int str, index, index1, count;
 	char *pointer;
-	unsigned int index, index1, size1, size2;
 
-	if (ac == 0 || *av == NULL)
-		return (NULL)
+	count = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (index = 0; index < ac; index++)
+	{
+		index1 = 0;
+		while (av[index][index1] != 0)
+			index1++;
+		count = count + index1;
+	}
+	index1 = 0;
+	pointer = malloc(sizeof(char) * (ac + count + 1));
 	if (pointer == NULL)
 		return (NULL);
+	for (index = 0; index < ac; index++)
+	{
+		str = 0;
+		while (av[index][str] != 0)
+		{
+			pointer[index1] = av[index][str];
+			str++;
+			index1++;
+		}
+		pointer[index1] = '\n';
+		index1++;
+	}
+	pointer[index1] = 0;
 	return (pointer);
 }
