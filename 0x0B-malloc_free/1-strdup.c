28b80c8210e8539deaa504ff54bb5d650cef4de6
Dany <dany.mrtinez@gmail.com>
2019-10-16 00:07:59 +0000

    add 1 taks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+  * create_array - returns a pointer to a newly allocated space in memory,
+  * which contains a copy of the string given as a parameter.
+  * @str: string.
+  *
+  * Return: NULL or String.
+  */
+char *_strdup(char *str)
+{
+	char *pointer;
+	unsigned int index, size;
+
+	if (str == NULL)
+		return (NULL);
+	for(size = 0; str[size] != 0; size++)
+	{
+	}
+	pointer = malloc(size * sizeof(char));
+	if (pointer == NULL)
+		return (NULL);
+	for (index = 0; index <= size; index++)
+		*(pointer + index) = str[index];
+	return (pointer);
+}
