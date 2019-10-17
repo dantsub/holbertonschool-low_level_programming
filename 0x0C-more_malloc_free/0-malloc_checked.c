c54dee11392ad803159b10c25689331f6b7d90fb
Dany <dany.mrtinez@gmail.com>
2019-10-17 23:28:05 +0000

    start project

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * malloc_checked - allocates memory using malloc.
+  * @b: bytes.
+  *
+  * Return: 98 or allocate memory.
+  */
+void *malloc_checked(unsigned int b)
+{
+	int *pointer;
+
+	pointer = malloc(b);
+	if (pointer == NULL)
+		exit(98);
+	return (pointer);
+}
