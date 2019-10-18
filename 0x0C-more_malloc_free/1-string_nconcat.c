9eca7a606631bfaa6a29350f68cbd6614517c07d
Dany <dany.mrtinez@gmail.com>
2019-10-18 00:24:57 +0000

    add 1 task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+#include <stdlib.h>
+/**
+  * string_nconcat - .
+  * @s1: First String.
+  * @s2: Second String.
+  * @n: size.
+  * Return: Concatenate String.
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pointer;
+	unsigned int index, index1, size1, size2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (size1 = 0; s1[size1] != 0; size1++)
+	{
+	}
+	for (size2 = 0; s2[size2] != 0; size2++)
+	{
+	}
+	if (n >= size2)
+		n = size2;
+	pointer = malloc((size1 + n) * sizeof(char) + 1);
+	if (pointer == NULL)
+		return (NULL);
+	for (index = 0; index < size1; index++)
+		pointer[index] = s1[index];
+	for (index1 = 0; index1 < n; index1++)
+		pointer[size1 + index1] = s2[index1];
+	return (pointer);
+}
