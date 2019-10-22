260dadbe4dbfdda7f18e568c03af7e9afd5fffed
Dany <dany.mrtinez@gmail.com>
2019-10-22 00:07:51 +0000

    try

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
 
-	d = malloc(sizeof(*d));
+	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
 	d->name = name;
