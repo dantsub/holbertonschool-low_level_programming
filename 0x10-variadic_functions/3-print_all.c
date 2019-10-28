c6097f4986f46144e89aa7fda17e0870f3c5abd1
dantsub <dany.mrtinez@gmail.com>
2019-10-28 02:00:29 +0000

    change the code for checker ¬¬

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,77 +1,51 @@
 #include "variadic_functions.h"
+
 /**
-  * print_all - prints anything.
-  * @format: format.
-  *
+  *print_all - Print all.
+  *@format: const.
   *
   */
+
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	int index, index1;
-
-	type_t args[] = {
-		{"s", print_s},
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{NULL, NULL}
-	};
-
-	va_start(arg, format);
-	index = 0;
-	while (format[index] != 0 && format != NULL)
-	{
-		index1 = 0;
-		while (index1 < 4)
-		{
-			if (format[index] == *(args[index1]).types)
-			{
-				args[index1].ftype(arg);
-				if (format[index + 1])
-					printf(", ");
-			}
-			index1++;
-		}
-		index++;
-	}
-	va_end(arg);
-	printf("\n");
-}
-/**
-  * print_s - print strings.
-  * @arg: argument.
-  */
-void print_s(va_list arg)
-{
 	char *string;
+	va_list arg;
+	unsigned int com, index;
 
-	string = va_arg(arg, char *);
-	if (string == NULL)
-		string = "(nil)";
-	printf("%s", string);
-}
-/**
-  * print_c - print char.
-  * @arg: argument.
-  */
-void print_c(va_list arg)
-{
-	printf("%c", va_arg(arg, int));
-}
-/**
-  * print_i - print interger.
-  * @arg: argument.
-  */
-void print_i(va_list arg)
-{
-	printf("%d", va_arg(arg, int));
-}
-/**
-  * print_f - print float.
-  * @arg: argument.
-  */
-void print_f(va_list arg)
-{
-	printf("%f", va_arg(arg, double));
+	index = 0;
+	while (format != NULL)
+	{
+		va_start(arg, format);
+		while (format[index] != 0)
+		{
+			com = 1;
+			switch (format[index])
+			{
+				case 'c':
+				printf("%c", va_arg(arg, int));
+				break;
+				case 'i':
+				printf("%d", va_arg(arg, int));
+				break;
+				case 'f':
+				printf("%f", va_arg(arg, double));
+				break;
+				case 's':
+				string = va_arg(arg, char *);
+				if (string == 0)
+				string = "(nil)";
+				printf("%s", string);
+				break;
+				default:
+				com = 0;
+				break;
+			}
+			if (format[index + 1] && com)
+				printf(", ");
+			index++;
+		}
+		va_end(arg);
+		break;
+	}
+	printf("\n");
 }
