c5e46a73fa40d294a24b85236bba6817b433258a
Dany <dany.mrtinez@gmail.com>
2019-11-13 22:11:06 +0000

    fix 3 task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	int wr, rd, file1, file2;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: %s %s\n", argv[1], argv[2]), exit(97);
+		dprintf(STDERR_FILENO, "Usage: file_from file_to\n"), exit(97);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 < 0)
 	{
