1823a1f88caa619d01588bb18cc729d45271945d
Dany <dany.mrtinez@gmail.com>
2019-11-13 01:25:09 +0000

    create file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * main - main.
+  * @argc: count.
+  * @argv: value.
+  * Return: -1 or 0.
+  */
+int main(int argc, char *argv[])
+{
+	if (!argv)
+		return (-1);
+	if (argc < 1 || argc > 3)
+		return (-1);
+	return (0);
+}
