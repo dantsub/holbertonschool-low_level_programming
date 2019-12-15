47ff94426484b2fe9e2f9adbb9bf184112d34a46
dantsub <dany.mrtinez@gmail.com>
2019-12-15 18:37:05 +0000

    change prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = *head;
+	
+	if (!(*head))
+		new_node->next = NULL;
+	else
+	{	new_node->next = *head;
+		(*head)->prev = new_node;
+	}
 	*head = new_node;
-
 	return (new_node);
 }
