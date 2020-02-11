ef64020544a721f8d042283e8bd031ecea3123a4
dantsub <dany.mrtinez@gmail.com>
2020-02-11 02:51:15 +0000

    betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-  * times_table - function that prints the n times table, starting with 0.
+  * print_times_table - function that prints the n times table, starting with 0
   * @n: times print table.
   * Return: nothing.
   */
