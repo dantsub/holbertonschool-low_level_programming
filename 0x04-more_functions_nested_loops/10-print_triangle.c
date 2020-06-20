daf671369ebf3feff3fb1e3f8fcb44d7f925c0e8
dantsub <dany.mrtinez@gmail.com>
2020-06-20 17:07:55 -0500

    [update] refactory

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,20 +10,17 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size > 0)
+		_putchar(10);
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				k = i + j;
-				if (k < size - 1)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
+			k = i + j;
+			if (k < size - 1)
+				_putchar(32);
+			else
+				_putchar('#');
 		}
+		_putchar(10);
 	}
-	else
-		_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,23 +9,17 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n < 1)
+		_putchar(10);
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		j = i;
+		while (j != 0 && j < n)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (i == j)
-				{
-					_putchar(92);
-					break;
-				}
-				else
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar(32);
+			j--;
 		}
+		_putchar(92);
+		_putchar(10);
 	}
-	else
-		_putchar('\n');
 }
