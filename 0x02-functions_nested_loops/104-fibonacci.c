4da149cd0aa81165d03d9d02baa4d179c5fbf617
dantsub <dany.mrtinez@gmail.com>
2020-06-20 14:02:00 -0500

    [fix] correct output

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long fibo1 = 0, fibo2 = 1, sum, mid1, mid2;
 	unsigned long fibo1_a, fibo1_b, fibo2_a, fibo2_b;
 
-	for (i = 0; i < 92; i++)
+	for (i = 0; i < 91; i++)
 	{
 		sum = fibo1 + fibo2;
 		printf("%lu, ", sum);
@@ -23,22 +23,23 @@ int main(void)
 	fibo1_b = fibo1 % 10000000000;
 	fibo2_b = fibo2 % 10000000000;
 
-	while (i < 99)
+	while (i < 98)
 	{
 		mid1 = fibo1_a + fibo2_a;
 		mid2 = fibo1_b + fibo2_b;
-		if (mid2 >= 10000000000)
+		if (mid2 > 10000000000)
 		{
 			mid1 += 1;
 			mid2 %= 10000000000;
 		}
+		
+		printf("%lu%lu", mid1, mid2);
+		if (i != 97)
+			printf(", ");
 		fibo1_a = fibo2_a;
 		fibo1_b = fibo2_b;
 		fibo2_a = mid1;
 		fibo2_b = mid2;
-		printf("%lu%lu", mid1, mid2);
-		if (i != 98)
-			printf(", ");
 		i++;
 	}
 	putchar(10);
