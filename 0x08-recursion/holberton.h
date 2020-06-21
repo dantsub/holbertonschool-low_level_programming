4574611927975e21561ad659612fe904636f2f69
dantsub <dany.mrtinez@gmail.com>
2020-06-21 00:59:30 -0500

    [update] add last task

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,5 +1,5 @@
-#ifndef holberton
-#define holberton
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,4 +9,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-#endif
+int wildcmp(char *s1, char *s2);
+#endif /* HOLBERTON_H */
